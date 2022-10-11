9d3baca1e60aa2c05a02b7f897e324c876bf236b
juyeon1031 <113417188+juyeon1031@users.noreply.github.com>
2022-10-11 16:44:30 +0900

    02

--- main.c
+++ main.c
@@ -3,36 +3,15 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-/*
-	int main(void) {
-	int i;
-	
-	for(i=0;i<10;i++)
-		printf("*");
-		
-	for(i=0;i<10;i++)
-		printf("*");
-		
-	for(i=0;i<10;i++)
-		printf("*");
+void func1(int x){
 
+	printf("func1 x is at %p\n", &x);
+}
+
+int main(void){
+	int x;
+	printf("main x is at %p\n", &x);
+	func1(x);
+	
 	return 0;
 }
-*/
-
-	void print_star()
-	{
-	 int i;
-	 for (i=0;i<10;i++)
-	 	printf("*");
-	}
-
-	int main(void)
-	{
-		print_star();
-		print_star();
-		print_star();
-		
-		return 0;
-	}
-
